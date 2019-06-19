976fa2372abed6ec06f36ff42a9ca9265220c1a4
皮志云 <pizhiyun@tsa.cn>
2019-06-19 09:58:00 +0800

    add all

---
+++ src/main/java/org/springboot/bookstore/BookStoreApplication.java
@@ -0,0 +1,14 @@
+package org.springboot.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 应用启动类
+ */
+@SpringBootApplication
+public class BookStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class, args);
+    }
+}
---
+++ src/main/java/org/springboot/bookstore/domain/Book.java
@@ -0,0 +1,75 @@
+package org.springboot.bookstore.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Book 实体类
+ */
+public class Book implements Serializable {
+
+    /**
+     * 编号
+     */
+    private Long id;
+
+    /**
+     * 书名
+     */
+    private String name;
+
+    /**
+     * 作者
+     */
+    private String writer;
+
+    /**
+     * 简介
+     */
+    private String introduction;
+
+    /**
+     * 出版日期
+     */
+    private Date date;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
